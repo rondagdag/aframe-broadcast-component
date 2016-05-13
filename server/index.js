767ec903e3709f6b23ade053daa35cd4900a2b52
Kevin Ngo <me@ngokevin.com>
2016-05-12 17:45:16 -0700

    move server to here

---
+++ server/index.js
@@ -0,0 +1,15 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 12000;
+server.listen(port);
+console.log('Server started on port', port);
+
+io.on('connection', function (socket) {
+  console.log('Connection received');
+
+  socket.on('broadcast', function (data) {
+    console.log('Broadcasting', data);
+    socket.broadcast.emit('broadcast', data);
+  });
+});
