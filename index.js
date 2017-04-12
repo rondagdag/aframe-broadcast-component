9ac6143ee2a934a460727cf8b6459adee3381f33
Ron Dagdag <ron@dagdag.net>
2017-04-12 15:18:30 -0500

    updated for 0.5.0

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (typeof AFRAME === 'undefined') {
 AFRAME.registerSystem('broadcast', {
   init: function () {
     var sceneEl = this.sceneEl;
-    var url = sceneEl.getAttribute('broadcast').url;
+    var url = sceneEl.getAttribute('Broadcast').url;
     this.socket = io(url);
 
     this.socket.on('connect', function () {
